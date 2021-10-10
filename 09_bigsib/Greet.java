cc5c478867bbf6d3039e82c997e8df99b9511ccf
Vansh Saboo <58910619+49PES@users.noreply.github.com>
2021-10-10 13:44:58 -0400

    Create Greet.java

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,21 @@
+/**
+Team: Baked Potatoes 
+Members (and their respective duckies): Emily Ortiz and Apple Juice, Vansh Saboo and Tiffany, Jacob Ng and Preguac
+APCS
+HW09 -- What Are BigSibs Good For?
+2021-10-02
+**/
+
+/*
+DISCOVERIES:
+UNRESOLVED QUESTIONS: 
+- What's the point of using these classes instead of making all the methods in one class? Purely organization purposes, or are there functional purposes as well?
+*/
+
+public class Greet{
+    public static void main( String[] args ) {
+        BigSib.greet("Flim");
+        BigSib.greet("Flam");
+        BigSib.greet("Kazaam");
+    }
+}
