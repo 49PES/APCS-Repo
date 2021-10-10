2a0c053d48b6fdf875e78a37b26b5dc7168861a0
Vansh Saboo <58910619+49PES@users.noreply.github.com>
2021-10-10 13:56:09 -0400

    Create BigSib.java

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,19 @@
+/*
+Team: Baked Potatoes 
+Members (and their respective duckies): Emily Ortiz and Apple Juice, Vansh Saboo and Tiffany, Jacob Ng and Preguac
+APCS
+HW10 -- Refactor Big Sib One
+2021-10-04
+*/
+
+/*
+DISCOVERIES:
+UNRESOLVED QUESTIONS:
+*/
+
+public class BigSib{
+    public static void main(String[] args){}
+    public static String greet(String name){
+        return "Why, hello there, " + name + ". How do you do?";
+    }
+}
