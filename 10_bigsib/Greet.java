c0c2c9e6aeb26482d3d71f03fb0be87c5b0b27f5
Vansh Saboo <58910619+49PES@users.noreply.github.com>
2021-10-10 13:52:22 -0400

    Create Greet.java

---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,15 @@
+/*
+Team: Baked Potatoes 
+Members (and their respective duckies): Emily Ortiz and Apple Juice, Vansh Saboo and Tiffany, Jacob Ng and Preguac
+APCS
+HW10 -- Refactor Big Sib One
+2021-10-04
+*/
+
+public class Greet {
+    public static void main( String[] args ){
+        System.out.println( BigSib.greet("Crosby") );
+        System.out.println( BigSib.greet("Stills") );
+        System.out.println( BigSib.greet("Nash") );
+    }
+}
