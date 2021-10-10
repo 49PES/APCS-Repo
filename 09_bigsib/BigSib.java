2a962df97f25505f997644206e82f83ac1829b86
Vansh Saboo <58910619+49PES@users.noreply.github.com>
2021-10-10 13:49:30 -0400

    Create BigSib.java

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,18 @@
+/**
+Team: Baked Potatoes 
+Members (and their respective duckies): Emily Ortiz and Apple Juice, Vansh Saboo and Tiffany, Jacob Ng and Preguac
+APCS
+HW09 -- What Are BigSibs Good For?
+2021-10-02
+**/
+/*
+DISCOVERIES:
+- You need a main() in a class, even if you do not use it (such as here)
+UNRESOLVED QUESTIONS:
+*/
+public class BigSib{
+  public static void main(String[] args){}
+  public static void greet(String name){
+    System.out.println("Why, hello there, " + name + ". How do you do?");
+  }
+}
