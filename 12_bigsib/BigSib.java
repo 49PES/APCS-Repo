85ecd431d91b3e2478857193313f57c21050c725
Vansh Saboo <58910619+49PES@users.noreply.github.com>
2021-10-10 14:08:26 -0400

    Create BigSib.java

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,21 @@
+/*
+Vansh Saboo and Abdullah Faruque
+HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
+2021-10-05
+*/
+
+/* 
+DICSO:
+- Objects are instances of a class. Woah. Objects.
+QCC:
+*/
+
+
+public class BigSib{
+
+	public static String greet(String name){return name;}
+	
+	public static void setHelloMsg(String msg){System.out.print(msg + " "); }
+  
+  public static void main(String[] args){}
+}
