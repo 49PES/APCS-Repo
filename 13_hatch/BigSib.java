82401b7cd44adcf4d83e0cca9b6b6850dd996a83
Vansh Saboo <58910619+49PES@users.noreply.github.com>
2021-10-10 14:15:25 -0400

    Create BigSib.java

---
+++ 13_hatch/BigSib.java
@@ -0,0 +1,30 @@
+
+/*
+Vansh Saboo and Abdullah Faruque
+HW13 -- Where do BigSibs Come From?
+2021-10-08
+*/
+
+/* 
+DISCO:
+QCC:
+*/
+
+public class BigSib{
+	String welcomeMsg;
+	public BigSib() {
+		welcomeMsg = "Word up ";
+	}
+	
+	public String greet(String name){
+			return welcomeMsg + name;
+	}
+	
+	
+	/*
+	public static void setHelloMsg(String msg){
+    String helloMsg = msg;
+  }
+	*/
+  
+ }
