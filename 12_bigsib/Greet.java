4fb58761a87649f010b11ef05c7507c050dfe504
Vansh Saboo <58910619+49PES@users.noreply.github.com>
2021-10-10 14:00:47 -0400

    Create Greet.java

---
+++ 12_bigsib/Greet.java
@@ -0,0 +1,24 @@
+
+/*
+Vansh Saboo and Abdullah Faruque
+HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
+2021-10-05
+*/
+
+/* 
+DISCOVERIES:
+UNRESOLVED QUESTIONS:
+*/
+
+public class Greet {
+  public static void main( String[] args ) {
+    String greeting;
+
+    BigSib richard = new BigSib();
+    richard.setHelloMsg("Word up");
+
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+
+  } //end main()
+} //end Greet
