d6176140215585ec13c5098e946ca637129fd5a2
Vansh Saboo <58910619+49PES@users.noreply.github.com>
2021-11-01 00:24:50 -0400

    Create helloName.java

---
+++ cb/String-1/helloName.java
@@ -0,0 +1,12 @@
+/* 
+Vansh Saboo
+APCS
+HW28 -- PPMP/Strings/
+Utilizing Strings for Coding Bat Problems
+2021-10-31
+time spent: 0.5 hours
+*/
+
+public String helloName(String name) {
+  return "Hello " + name + "!";
+} 
