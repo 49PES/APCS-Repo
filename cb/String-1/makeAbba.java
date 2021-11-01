c05b92618a32c98d345db72a5aa5da5de99edf8f
Vansh Saboo <58910619+49PES@users.noreply.github.com>
2021-11-01 00:25:45 -0400

    Create makeAbba.java

---
+++ cb/String-1/makeAbba.java
@@ -0,0 +1,12 @@
+/*
+Vansh Saboo
+APCS
+HW28 -- PPMP/Strings/
+Utilizing Strings for Coding Bat Problems
+2021-10-31
+time spent: 0.5 hours
+*/
+
+public String makeAbba(String a, String b) {
+  return a + b + b + a;
+}
