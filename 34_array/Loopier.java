a26138d9edb59c19ec79bce6ff3aed240c4a6684
Vansh Saboo <58910619+49PES@users.noreply.github.com>
2021-11-14 22:14:40 -0500

    Update Loopier.java

--- 34_array/Loopier.java
+++ 34_array/Loopier.java
@@ -1,87 +1,132 @@
-/* Vansh Saboo, Jason Zhuo
-APCS
-HW34 -- A Pirate’s Life for Me / Arrays 
-Methods for turning arrays into strings, and methods for linear search and frequency counters (iterative and recursive)
-2021-11-14
-time spent: 0.5 hours
+/* 
+   Silly Serpants
+   Humans: Jason Zhou, Vansh Saboo; Duckies: Tiggy, Tiffany
+   APCS
+   HW34: A Pirate's Life for Me (Arrays - finding freq and index)
+   2021-14-11
+   time spent: 0.3 hours
 
-DISCO:
+
 QCC: 
-- I feel as though the linSearchR method could have been made easier to interpret and implement had there been three parameters (an index parameter to keep track) - would we be allowed to add more parameters?
+DISCO: Recursion is more annoying to do in these cases, Array.copyOfRange
+       Vansh - I feel as though the linSearchR method could have been made
+       easier to interpret and implement had there been three parameters 
+       (an index parameter to keep track) - would we be allowed to add 
+       more parameters
 */
 
-public class Loopier{
-	public static int[] generator(int[] a){
-	    for (int i = 0; i < a.length ; i++){a[i] = (int) (Math.random() * Math.pow(2.0, 31.0));} // 
-	    return a;
-	}
-	
-	public static String stringify(int[] a){
-	    String output = "[";
-	    if(a.length > 0){
-	        output += a[0];
-	        for (int i = 1; i < a.length ; i++){
-	            output += ", " + a[i];
-	        }
-	    }
-	    return output + "]";
-	}
-	
-	public static int linSearch(int[] a, int target){
-	    for(int i = 0; i < a.length ; i++){
-	        if(a[i] == target){return i;}
-	    }
-	    return -1;
-	}
-	
-	public static int freq(int[] a, int target){
-	    int counter = 0;
-	    for(int i = 0; i < a.length; i++){
-	        if(a[i] == target){counter++;}
-	    }
-	    return counter;
-	}
-    
-    // Method for recursive reduction - generates subarrays with elements of indexes 1+
-    // Could be replaced with Arrays.copyOfRange() method from Array library
-    
-	public static int[] copier(int[] a){
-	    int[] copy = new int[a.length - 1];
-	    for(int i = 1; i < a.length; i++){
-	        copy[i - 1] = a[i];
-	    }
-	    return copy;
-	}
-	
-	public static int linSearchR(int[] a, int target){
-	    if(a[0] == target){return 0;}
+import java.util.Arrays;
+
+public class Looper {
+
+  public static int[] copy(int[] a, int start, int end) {
+    int[] b = new int[end - start + 1];
+    int i = 0;
+    while (start <= end) {
+      b[i] = a[start];
+      start++;
+      i++;
+    }
+    return b;
+  }
+
+  public static int[] copier(int[] a){
+    int[] copy = new int[a.length - 1];
+    for(int i = 1; i < a.length; i++){
+        copy[i - 1] = a[i];
+    }
+    return copy;
+  }
+  //current values from -100 to 100
+  public static void populateArrayWithRandom(int[] array) {
+    for (int i = 0; i < array.length; i++) {
+      array[i] = (Math.random() < 0.5) ? (int) (Math.random() * Math.pow(2, 31)) : (int) (Math.random() * Math.pow(2, 31) * -1);
+    }
+  }
+  
+  public static String arrayToString(int[] array) {
+    String stringThing = "";
+    for (int i = 0; i < array.length; i++) {
+        stringThing += array[i] + " ";
+    }
+    return stringThing;
+  }
+
+  public static int linSearch(int[] a, int target) {
+    for (int i = 0; i < a.length; i++) {
+      if (target == a[i]) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  
+  public static int linSearchR(int[] a, int target) {
+    if(a[0] == target){return 0;}
 	    if(a.length == 1){return -1;}
 	    int index = linSearchR(copier(a), target);
 	    if (index == -1){return -1;}
 	    return index + 1;
-	}
+  }
+
+  public static int freq(int[] a, int target) {
+    int counter = 0;
+    for (int i = 0; i < a.length; i++) {
+      if (target == a[i]) {
+        counter++;
+      }
+    }
+    return counter;
+  }
   
-	public static int freqRec(int[] a, int target){
-	    if(a.length == 0){return 0;}
-	    if(a[0] == target){return 1 + freqRec(copier(a), target);}
-	    return freqRec(copier(a), target);
-	}
-	
-	public static void main(String[] args) {
-		int[] scores = new int[5];
-		System.out.println(stringify(generator(scores)));
-		
-		System.out.println();
-		int[] values = {5, 5, 10, 9, 7};
-		System.out.println(linSearch(values, 8));
-		System.out.println(linSearch(values, 5));
-		System.out.println(linSearchR(values, 8));
-		System.out.println(linSearchR(values, 9));
-		
-		System.out.println();
-		System.out.println(freq(values, 10));
-		System.out.println(freqRec(values, 10));
-		System.out.println(freq(values, 1029));
-		System.out.println(freqRec(values, 1029));
-	}
+  public static int freqRec(int[] a, int target) {
+    if (a.length == 0) {
+      return 0;
+    } else if (a[0] == target) {
+      return 1 + freqRec(Arrays.copyOfRange(a, 1, a.length), target);
+    } else {
+      return freqRec(Arrays.copyOfRange(a, 1, a.length), target);
+    }
+  }
+  public static void main(String[] args) {
+      
+      int[] array = {9, 9, 6, 7, 8, 9, 9, 9};
+      int[] array2 = {4};
+      int[] array3 = {2, 3, 4, 5};
+      
+      /*
+      for (int i : array) {
+        System.out.println(linSearchR(array, i));
+      }
+      */
+      System.out.println(arrayToString(copy(array, 0, 5))); // 9, 9, 6, 7, 8, 9
+      System.out.println(arrayToString(Arrays.copyOfRange(array, 0, array.length - 1))); // 9, 9, 6, 7, 8, 9
+      System.out.println(linSearchR(array, 9)); // 0 
+      System.out.println(linSearchR(array2, 9)); // -1
+      System.out.println(linSearchR(array3, 9)); // -1
+      System.out.println(linSearchR(array3, 4)); // 2
+      System.out.println(linSearchR(array3, 3)); // 1
+      System.out.println(linSearch(array, 9)); // 0
+      System.out.println(linSearch(array2, 9)); // -1
+      System.out.println(linSearch(array3, 3)); // 1
+      System.out.println(freqRec(array, 9)); // 5
+      System.out.println(freqRec(array, 21)); // 0
+      System.out.println(freq(array, 9)); // 5
+
+      int[] scores = new int[5];
+      System.out.println(stringify(generator(scores)));
+      
+      System.out.println();
+      int[] values = {5, 5, 10, 9, 7};
+      System.out.println(linSearch(values, 8));
+      System.out.println(linSearch(values, 5));
+      System.out.println(linSearchR(values, 8));
+      System.out.println(linSearchR(values, 9));
+      
+      System.out.println();
+      System.out.println(freq(values, 10));
+      System.out.println(freqRec(values, 10));
+      System.out.println(freq(values, 1029));
+      System.out.println(freqRec(values, 1029));
+    }
 }
